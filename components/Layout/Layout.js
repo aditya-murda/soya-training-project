3a1a9599f4eaeb5f4930892cd2da29b4193b051f
Aditya Murda <aditya.nugraha@traveloka.com>
2018-08-25 12:16:39 +0700

    change application id

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,17 +1,38 @@
+import React from 'react';
+import { Icon, NotificationContainer } from "@traveloka/soya-components";
+import { OIDCLayout } from '@traveloka/soya-components/ath';
+import Link from 'next/link'
+import "@traveloka/soya-components/styles";
 import Head from "next/head";
 import PropTypes from "prop-types";
 import logo from "./logo.svg";
 import styles from "./Layout.module.css";
 import "../../styles/global.css";
 
+const menus = () => [
+  {
+    title: "Bitcoin",
+    iconText: <Icon icon="home"/>,
+    links: [
+      <Link href="/"><a><Icon icon="dashboard"/>Dashboard</a></Link>
+    ]
+  },
+  {
+    title: "Master",
+    iconText: <Icon icon="apps"/>,
+    links: [
+      <Link href="/currencies"><a><Icon icon="local-atm"/>Currencies</a></Link>,
+      <Link href="settings"><a><Icon icon="settings"/>Settings</a></Link>
+    ]
+  }
+];
+
 const Layout = ({ children }) => (
-  <div className={styles.layout}>
-    <Head>
-      <link rel="shortcut icon" href="/static/favicon.ico" />
-    </Head>
-    <img src={logo} />
+  <OIDCLayout menuData={menus()}>
+    <Head><link href="/static/favicon.ico"/></Head>
+    <NotificationContainer />
     {children}
-  </div>
+  </OIDCLayout>
 );
 
 Layout.propTypes = {
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import Head from "next/head";
 import { createPage } from "soya-next";
 import Layout from "../components/Layout/Layout";
+import React from 'react';
+import { createPageWithOIDCAuth, OIDCAuthorize } from '@traveloka/soya-components/ath';
+import {Breadcrumb, Select, Label} from "@traveloka/soya-components";
+import Home from "../containers/Home";
+
+let title = "Bitcoin";
+let heading = "Home";
 
 const IndexPage = () => (
+  <OIDCAuthorize>
   <Layout>
     <Head>
-      <title>Bitcoin</title>
+      <title>{title}</title>
+      <h1>{heading}</h1>
     </Head>
-    <p>mroemzh</p>
+    <Home />
   </Layout>
+  </OIDCAuthorize>
 );
 
-export default createPage()(IndexPage);
+export default createPageWithOIDCAuth()(IndexPage);
